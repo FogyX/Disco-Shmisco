ac16124dd539117161a660885a3eddb4fd44e75a
FogyX <maksdima0515@gmail.com>
2022-05-31 12:35:25 +0500

    Game Over. BEGIN.

--- Assets/MyAssets/Scripts/MainMenuScripts/FunnyButtonTouch.cs
+++ Assets/MyAssets/Scripts/MainMenuScripts/FunnyButtonTouch.cs
@@ -6,11 +6,6 @@ public class FunnyButtonTouch : MonoBehaviour
 
     [SerializeField] private AudioSource audioSource;
 
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-
     public void PlayFunnySound()
     {
         audioSource.PlayOneShot(audioClip);
